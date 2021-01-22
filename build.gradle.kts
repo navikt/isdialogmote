676c68799cdbeb4f8030dd7b2e0b377bfbdc1340
Erik Jansen <erik.gunnar.jansen@nav.no>
2021-01-22 10:47:57 +0100

    Init Ktor dependencies and add Dockerfile

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,22 @@
-plugins {
-    id("org.jetbrains.kotlin.jvm") version "1.3.41"
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+import com.github.jengelman.gradle.plugins.shadow.transformers.ServiceFileTransformer
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-    application
+group = "no.nav.syfo"
+version = "1.0.0"
+
+object Versions {
+    const val coroutinesVersion = "1.4.2"
+    const val jacksonVersion = "2.11.3"
+    const val ktorVersion = "1.5.0"
+    const val logbackVersion = "1.2.3"
+    const val prometheusVersion = "0.9.0"
+}
+
+plugins {
+    kotlin("jvm") version "1.4.21"
+    id("com.github.johnrengelman.shadow") version "6.1.0"
+    id("org.jlleitschuh.gradle.ktlint") version "9.4.1"
 }
 
 repositories {
@@ -9,14 +24,39 @@ repositories {
 }
 
 dependencies {
-    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation(kotlin("stdlib"))
+    implementation(kotlin("reflect"))
 
-    testImplementation("org.jetbrains.kotlin:kotlin-test")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
+    implementation("io.ktor:ktor-server-netty:${Versions.ktorVersion}")
+
+    implementation("io.prometheus:simpleclient_hotspot:${Versions.prometheusVersion}")
+    implementation("io.prometheus:simpleclient_common:${Versions.prometheusVersion}")
+
+    implementation("ch.qos.logback:logback-classic:${Versions.logbackVersion}")
 }
 
-application {
-    // Define the main class for the application
-    mainClassName = "isdialogmote.AppKt"
+tasks {
+    withType<Jar> {
+        manifest.attributes["Main-Class"] = "no.nav.syfo.AppKt"
+    }
+
+    create("printVersion") {
+        doLast {
+            println(project.version)
+        }
+    }
+
+    withType<KotlinCompile> {
+        kotlinOptions.jvmTarget = "11"
+    }
+
+    withType<ShadowJar> {
+    }
+
+    withType<Test> {
+        useJUnitPlatform {
+            includeEngines("spek2")
+        }
+        testLogging.showStandardStreams = true
+    }
 }
