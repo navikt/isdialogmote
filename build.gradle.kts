b19667f0424a4c73e384ad23efb90c5a1a76715f
Erik Jansen <erik.gunnar.jansen@nav.no>
2021-01-22 10:39:32 +0100

    Initialize Gradle

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    id("org.jetbrains.kotlin.jvm") version "1.3.41"
+
+    application
+}
+
+repositories {
+    jcenter()
+}
+
+dependencies {
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
+}
+
+application {
+    // Define the main class for the application
+    mainClassName = "isdialogmote.AppKt"
+}
