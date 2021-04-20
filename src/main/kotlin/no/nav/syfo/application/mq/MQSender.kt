170fb3db2c359467b822118b51ce78e4d0313754
geir-waagboe <geir.arne.waagbo@nav.no>
2021-04-20 11:26:32 +0200

    Førsøker med en annen connection factory

--- src/main/kotlin/no/nav/syfo/application/mq/MQSender.kt
+++ src/main/kotlin/no/nav/syfo/application/mq/MQSender.kt
@@ -1,15 +1,13 @@
 package no.nav.syfo.application.mq
 
-import com.ibm.mq.constants.CMQC.MQENC_NATIVE
-import com.ibm.msg.client.jms.JmsConstants.JMS_IBM_CHARACTER_SET
-import com.ibm.msg.client.jms.JmsConstants.JMS_IBM_ENCODING
-import com.ibm.msg.client.jms.JmsFactoryFactory
-import com.ibm.msg.client.wmq.common.CommonConstants.*
+import com.ibm.mq.jms.MQConnectionFactory
+import com.ibm.msg.client.wmq.WMQConstants
+import com.ibm.msg.client.wmq.compat.base.internal.MQC
 import no.nav.syfo.application.Environment
 import no.nav.syfo.varsel.MotedeltakerVarselType
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import javax.jms.ConnectionFactory
+import javax.jms.Session
 
 private const val UTF_8_WITH_PUA = 1208
 private val log: Logger = LoggerFactory.getLogger("no.nav.syfo.application.mq")
@@ -23,10 +21,12 @@ class MQSender(private val env: Environment) : MQSenderInterface {
         val queueName = getQueueName(env)
         if (env.mqSendingEnabled) {
             log.info("Sending message of type $varseltype to $queueName where payload is\n $payload")
-            connectionFactory(env).createContext().use { jmsContext ->
-                val destination = jmsContext.createQueue("queue:///$queueName")
-                val message = jmsContext.createTextMessage(payload)
-                jmsContext.createProducer().send(destination, message)
+            connectionFactory(env).createConnection(env.mqUsername, env.mqPassword).use { connection ->
+                connection.createSession(false, Session.AUTO_ACKNOWLEDGE).use { session ->
+                    val destination = session.createQueue(queueName)
+                    val message = session.createTextMessage(payload)
+                    session.createProducer(destination).send(message)
+                }
             }
         } else {
             log.info("MQ-message sending disabled, would have sent message of type $varseltype to $queueName")
@@ -37,19 +37,14 @@ class MQSender(private val env: Environment) : MQSenderInterface {
         return env.mqTredjepartsVarselQueue
     }
 
-    private fun connectionFactory(env: Environment): ConnectionFactory {
-        return JmsFactoryFactory.getInstance(WMQ_PROVIDER).createConnectionFactory().apply {
-            setIntProperty(WMQ_CONNECTION_MODE, WMQ_CM_CLIENT)
-            setStringProperty(WMQ_QUEUE_MANAGER, env.mqQueueManager)
-            setStringProperty(WMQ_HOST_NAME, env.mqHostname)
-            setIntProperty(WMQ_PORT, env.mqPort)
-            setStringProperty(WMQ_CHANNEL, env.mqChannelName)
-            setIntProperty(WMQ_CCSID, UTF_8_WITH_PUA)
-            setIntProperty(JMS_IBM_ENCODING, MQENC_NATIVE)
-            setIntProperty(JMS_IBM_CHARACTER_SET, UTF_8_WITH_PUA)
-            setBooleanProperty(USER_AUTHENTICATION_MQCSP, true)
-            setStringProperty(USERID, env.mqUsername)
-            setStringProperty(PASSWORD, env.mqPassword)
-        }
+    private fun connectionFactory(config: Environment) = MQConnectionFactory().apply {
+        hostName = config.mqHostname
+        port = config.mqPort
+        queueManager = config.mqQueueManager
+        transportType = WMQConstants.WMQ_CM_CLIENT
+        channel = config.mqChannelName
+        ccsid = UTF_8_WITH_PUA
+        setIntProperty(WMQConstants.JMS_IBM_ENCODING, MQC.MQENC_NATIVE)
+        setIntProperty(WMQConstants.JMS_IBM_CHARACTER_SET, UTF_8_WITH_PUA)
     }
 }
