846102a384136f44d79faa9919b5d727c83d2f18
geir-waagboe <geir.arne.waagbo@nav.no>
2021-04-27 11:25:01 +0200

    Teste sende melding til MQ

--- src/main/kotlin/no/nav/syfo/App.kt
+++ src/main/kotlin/no/nav/syfo/App.kt
@@ -13,20 +13,26 @@ import no.nav.syfo.application.api.authentication.getWellKnown
 import no.nav.syfo.application.database.applicationDatabase
 import no.nav.syfo.application.database.databaseModule
 import no.nav.syfo.application.mq.MQSender
+import no.nav.syfo.client.narmesteleder.NarmesteLederDTO
+import no.nav.syfo.varsel.MotedeltakerVarselType
 import no.nav.syfo.varsel.arbeidstaker.brukernotifikasjon.BrukernotifikasjonProducer
 import no.nav.syfo.varsel.arbeidstaker.brukernotifikasjon.kafkaBrukernotifikasjonProducerConfig
+import no.nav.syfo.varsel.narmesteleder.NarmesteLederVarselService
 import org.apache.kafka.clients.producer.KafkaProducer
 import org.slf4j.LoggerFactory
+import java.time.LocalDate
+import java.time.LocalDateTime
 import java.util.concurrent.TimeUnit
 
 const val applicationPort = 8080
 
-val applicationState = ApplicationState(
-    alive = true,
-    ready = false
-)
 
 fun main() {
+    val applicationState = ApplicationState(
+        alive = true,
+        ready = false
+    )
+
     val server = embeddedServer(
         Netty,
         applicationEngineEnvironment {
@@ -61,6 +67,25 @@ fun main() {
                     wellKnownSelvbetjening = getWellKnown(environment.loginserviceIdportenDiscoveryUrl),
                     wellKnownVeileder = getWellKnown(environment.aadDiscoveryUrl),
                 )
+            }
+            val narmesteLederVarselService = NarmesteLederVarselService(
+                env = environment,
+                mqSender = mqSender
+            )
+
+            try {
+                val dto = NarmesteLederDTO(
+                    "Geir",
+                    epost = "geir.arne.waagbo@nav.no",
+                    tlf = "93248340",
+                    aktoerId = "",
+                    organisasjonsnavn = "NAV",
+                    fomDato = LocalDate.now(),
+                    orgnummer = "999999999"
+                )
+                narmesteLederVarselService.sendVarsel(LocalDateTime.now(), dto, MotedeltakerVarselType.INNKALT)
+            } catch (exc: Exception) {
+                log.warn("Tried sending MQ-message, got exception: ", exc)
             }
         }
     )
