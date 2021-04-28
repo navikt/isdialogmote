5dc7278f1445231ec7c6fe0e3858f0b4d30fcb79
geir-waagboe <geir.arne.waagbo@nav.no>
2021-04-28 09:40:17 +0200

    Forbedre liveness- og readiness-prober

--- src/test/kotlin/no/nav/syfo/application/api/PodApiSpek.kt
+++ src/test/kotlin/no/nav/syfo/application/api/PodApiSpek.kt
@@ -4,6 +4,8 @@ import io.ktor.http.*
 import io.ktor.routing.*
 import io.ktor.server.testing.*
 import no.nav.syfo.application.ApplicationState
+import no.nav.syfo.testhelper.TestDatabase
+import no.nav.syfo.testhelper.TestDatabaseNotResponding
 import org.amshove.kluent.shouldBeEqualTo
 import org.amshove.kluent.shouldNotBeEqualTo
 import org.spekframework.spek2.Spek
@@ -14,12 +16,14 @@ object PodApiSpek : Spek({
     describe("Successful liveness and readiness checks") {
         with(TestApplicationEngine()) {
             start()
+            val database = TestDatabase()
             application.routing {
                 registerPodApi(
                     ApplicationState(
                         alive = true,
                         ready = true
-                    )
+                    ),
+                    database,
                 )
             }
 
@@ -42,18 +46,49 @@ object PodApiSpek : Spek({
     describe("Unsuccessful liveness and readiness checks") {
         with(TestApplicationEngine()) {
             start()
+            val database = TestDatabase()
             application.routing {
                 registerPodApi(
                     ApplicationState(
                         alive = false,
                         ready = false
-                    )
+                    ),
+                    database,
                 )
             }
 
             it("Returns internal server error when liveness check fails") {
                 with(handleRequest(HttpMethod.Get, "/is_alive")) {
                     response.status() shouldBeEqualTo HttpStatusCode.InternalServerError
+                    response.content shouldNotBeEqualTo null
+                }
+            }
+
+            it("Returns internal server error when readiness check fails") {
+                with(handleRequest(HttpMethod.Get, "/is_ready")) {
+                    response.status() shouldBeEqualTo HttpStatusCode.InternalServerError
+                    response.content shouldNotBeEqualTo null
+                }
+            }
+        }
+    }
+    describe("Successful liveness and unsuccessful readiness checks when database not working") {
+        with(TestApplicationEngine()) {
+            start()
+            val database = TestDatabaseNotResponding()
+            application.routing {
+                registerPodApi(
+                    ApplicationState(
+                        alive = true,
+                        ready = true
+                    ),
+                    database,
+                )
+            }
+
+            it("Returns ok on is_alive") {
+                with(handleRequest(HttpMethod.Get, "/is_alive")) {
+                    response.status()?.isSuccess() shouldBeEqualTo true
                     response.content shouldNotBeEqualTo null
                 }
             }
