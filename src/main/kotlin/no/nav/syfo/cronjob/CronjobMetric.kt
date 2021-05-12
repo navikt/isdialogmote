c20a716b62db9f15f861338a667ec2f1aa2414b6
Erik Jansen <erik.gunnar.jansen@nav.no>
2021-05-12 13:15:48 +0200

    Add alert for failures in cronjob for Journalforing.

--- src/main/kotlin/no/nav/syfo/cronjob/CronjobMetric.kt
+++ src/main/kotlin/no/nav/syfo/cronjob/CronjobMetric.kt
@@ -3,11 +3,8 @@ package no.nav.syfo.cronjob
 import io.prometheus.client.Counter
 import no.nav.syfo.metric.METRICS_NS
 
-const val CRONJOB_BASE = "cronjob"
-const val CRONJOB_JOURNALFORING_BASE = "${CRONJOB_BASE}_journalforing"
-
-const val CRONJOB_JOURNALFORING_VARSEL_UPDATE = "${CRONJOB_JOURNALFORING_BASE}_update_count"
-const val CRONJOB_JOURNALFORING_VARSEL_FAIL = "${CRONJOB_JOURNALFORING_BASE}_fail_count"
+const val CRONJOB_JOURNALFORING_VARSEL_UPDATE = "cronjob_journalforing_update_count"
+const val CRONJOB_JOURNALFORING_VARSEL_FAIL = "cronjob_journalforing_fail_count"
 val COUNT_CRONJOB_JOURNALFORING_VARSEL_UPDATE: Counter = Counter.build()
     .namespace(METRICS_NS)
     .name(CRONJOB_JOURNALFORING_VARSEL_UPDATE)
