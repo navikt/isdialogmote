64087e9a8c2d4585fbcad7f77a0fa2c3fbb87c23
John Martin Lindseth <john.martin.lindseth@nav.no>
2021-06-03 11:44:32 +0200

    Handle ConnectionException in redis
    Hvis vi ikke får kontakt med redis, logg feilen og returner null
    Hvis vi ikke får lagret, logg feilen og fortsett som før.

--- src/main/kotlin/no/nav/syfo/application/cache/RedisStore.kt
+++ src/main/kotlin/no/nav/syfo/application/cache/RedisStore.kt
@@ -1,10 +1,14 @@
 package no.nav.syfo.application.cache
 
 import no.nav.syfo.util.configuredJacksonMapper
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import redis.clients.jedis.JedisPool
+import redis.clients.jedis.exceptions.JedisConnectionException
 
 class RedisStore(private val jedisPool: JedisPool) {
 
+    private val log: Logger = LoggerFactory.getLogger("no.nav.syfo.application.cache")
     val mapper = configuredJacksonMapper()
 
     inline fun <reified T> getObject(key: String): T? {
@@ -13,7 +17,12 @@ class RedisStore(private val jedisPool: JedisPool) {
     }
 
     fun get(key: String): String? {
-        jedisPool.resource.use { jedis -> return jedis.get(key) }
+        try {
+            jedisPool.resource.use { jedis -> return jedis.get(key) }
+        } catch (e: JedisConnectionException) {
+            log.warn("Got connection error when fetching from redis! Continuing without cached value", e)
+            return null
+        }
     }
 
     fun <T> setObject(key: String, value: T, expireSeconds: Int) {
@@ -21,12 +30,16 @@ class RedisStore(private val jedisPool: JedisPool) {
     }
 
     fun set(key: String, value: String, expireSeconds: Int) {
-        jedisPool.resource.use { jedis ->
-            jedis.setex(
-                key,
-                expireSeconds,
-                value,
-            )
+        try {
+            jedisPool.resource.use { jedis ->
+                jedis.setex(
+                    key,
+                    expireSeconds,
+                    value,
+                )
+            }
+        } catch (e: JedisConnectionException) {
+            log.warn("Got connection error when storing in redis! Continue without caching", e)
         }
     }
 }
