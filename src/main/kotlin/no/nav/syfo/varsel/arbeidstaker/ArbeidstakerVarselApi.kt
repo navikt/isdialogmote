d8d36a15e275f1c854b777693a60f37cdf5ab819
geir-waagboe <geir.arne.waagbo@nav.no>
2021-06-22 16:33:04 +0200

    Endepunkt for å hente ut pdf for arbeidstaker-varsel

--- src/main/kotlin/no/nav/syfo/varsel/arbeidstaker/ArbeidstakerVarselApi.kt
+++ src/main/kotlin/no/nav/syfo/varsel/arbeidstaker/ArbeidstakerVarselApi.kt
@@ -10,6 +10,7 @@ import no.nav.syfo.dialogmote.DialogmotedeltakerService
 import no.nav.syfo.dialogmote.domain.toArbeidstakerVarselDTOList
 import no.nav.syfo.util.callIdArgument
 import no.nav.syfo.util.getCallId
+import no.nav.syfo.varsel.arbeidstaker.domain.PdfContent
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import java.util.*
@@ -19,6 +20,7 @@ private val log: Logger = LoggerFactory.getLogger("no.nav.syfo")
 const val arbeidstakerVarselApiPath = "/api/v1/arbeidstaker/varsel"
 const val arbeidstakerVarselApiVarselParam = "varseluuid"
 const val arbeidstakerVarselApiLesPath = "/les"
+const val arbeidstakerVarselApiPdfPath = "/pdf"
 
 fun Route.registerArbeidstakerVarselApi(
     dialogmoteService: DialogmoteService,
@@ -37,6 +39,59 @@ fun Route.registerArbeidstakerVarselApi(
                 call.respond(arbeidstakerVarselDTOList)
             } catch (e: IllegalArgumentException) {
                 val illegalArgumentMessage = "Could not retrieve VarselList"
+                log.warn("$illegalArgumentMessage: {}, {}", e.message, callIdArgument(callId))
+                call.respond(HttpStatusCode.BadRequest, e.message ?: illegalArgumentMessage)
+            }
+        }
+
+        get("/{$arbeidstakerVarselApiVarselParam}$arbeidstakerVarselApiPdfPath") {
+            val callId = getCallId()
+            try {
+                val requestPersonIdent = call.personIdent()
+                    ?: throw IllegalArgumentException("No PersonIdent found in token")
+
+                val varselUuid = UUID.fromString(call.parameters[arbeidstakerVarselApiVarselParam])
+
+                val motedeltakerArbeidstakerVarsel = dialogmotedeltakerService.getDialogmoteDeltakerArbeidstakerVarselList(
+                    uuid = varselUuid
+                ).firstOrNull()
+
+                val referat = dialogmoteService.getReferat(
+                    referatUUID = varselUuid
+                )
+
+                if (motedeltakerArbeidstakerVarsel == null && referat == null) {
+                    throw IllegalArgumentException("No Varsel found for PersonIdent with uuid=$varselUuid")
+                }
+                val motedeltakerArbeidstakerId = if (referat != null) {
+                    dialogmoteService.getDialogmote(referat.moteId).arbeidstaker.id
+                } else {
+                    motedeltakerArbeidstakerVarsel!!.motedeltakerArbeidstakerId
+                }
+
+                val motedeltakerArbeidstaker = dialogmotedeltakerService.getDialogmoteDeltakerArbeidstakerFromId(
+                    moteDeltakerArbeidstakerId = motedeltakerArbeidstakerId
+                )
+
+                val hasAccessToVarsel = motedeltakerArbeidstaker.personIdent == requestPersonIdent
+                if (hasAccessToVarsel) {
+                    val pdf = if (referat != null) {
+                        dialogmotedeltakerService.getReferatPdf(
+                            referatUuid = varselUuid,
+                        )
+                    } else { // motedeltakerArbeidstakerVarsel != null
+                        dialogmotedeltakerService.getDialogmotedeltakerArbeidstakerVarselPdf(
+                            dialogmotedeltakerArbeidstakerVarselUuid = varselUuid,
+                        )
+                    }
+                    call.respond(PdfContent(pdf))
+                } else {
+                    val accessDeniedMessage = "Denied access to pdf for Varsel for varselUUID"
+                    log.warn("$accessDeniedMessage, {}", callIdArgument(callId))
+                    call.respond(HttpStatusCode.Forbidden, accessDeniedMessage)
+                }
+            } catch (e: IllegalArgumentException) {
+                val illegalArgumentMessage = "Could not get pdf for Varsel for varselUUID"
                 log.warn("$illegalArgumentMessage: {}, {}", e.message, callIdArgument(callId))
                 call.respond(HttpStatusCode.BadRequest, e.message ?: illegalArgumentMessage)
             }
