ecae3f9f1c92d36bf651542474aa58d3bee87172
Erik Jansen <erik.gunnar.jansen@nav.no>
2021-06-30 14:17:10 +0200

    Remove unused import

--- src/main/kotlin/no/nav/syfo/client/HttpClientCommon.kt
+++ src/main/kotlin/no/nav/syfo/client/HttpClientCommon.kt
@@ -3,7 +3,6 @@ package no.nav.syfo.client
 import io.ktor.client.*
 import io.ktor.client.engine.apache.*
 import io.ktor.client.engine.cio.*
-import io.ktor.client.features.*
 import io.ktor.client.features.json.*
 import no.nav.syfo.util.configuredJacksonMapper
 import org.apache.http.impl.conn.SystemDefaultRoutePlanner
