2529a6d4efa5480376fbf264e16124b151c8d88f
Erik Jansen <erik.gunnar.jansen@nav.no>
2021-09-03 15:58:06 +0200

    Reuse proxyConfig

--- src/main/kotlin/no/nav/syfo/application/api/authentication/OIDC.kt
+++ src/main/kotlin/no/nav/syfo/application/api/authentication/OIDC.kt
@@ -1,30 +1,17 @@
 package no.nav.syfo.application.api.authentication
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import io.ktor.client.*
 import io.ktor.client.engine.apache.*
-import io.ktor.client.features.json.*
 import io.ktor.client.request.*
 import kotlinx.coroutines.runBlocking
-import no.nav.syfo.util.configuredJacksonMapper
-import org.apache.http.impl.conn.SystemDefaultRoutePlanner
-import java.net.ProxySelector
+import no.nav.syfo.client.proxyConfig
 
-val proxyConfig: HttpClientConfig<ApacheEngineConfig>.() -> Unit = {
-    install(JsonFeature) {
-        serializer = JacksonSerializer(configuredJacksonMapper())
-    }
-    engine {
-        customizeClient {
-            setRoutePlanner(SystemDefaultRoutePlanner(ProxySelector.getDefault()))
-        }
+fun getWellKnown(wellKnownUrl: String) = runBlocking {
+    HttpClient(Apache, proxyConfig).use { client ->
+        client.get<WellKnown>(wellKnownUrl)
     }
 }
 
-fun getWellKnown(wellKnownUrl: String) =
-    runBlocking { HttpClient(Apache, proxyConfig).use { cli -> cli.get<WellKnown>(wellKnownUrl) } }
-
-@JsonIgnoreProperties(ignoreUnknown = true)
 data class WellKnown(
     val authorization_endpoint: String,
     val token_endpoint: String,
