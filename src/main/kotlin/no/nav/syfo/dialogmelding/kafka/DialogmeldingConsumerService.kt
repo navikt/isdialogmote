02d4f7fa8f7c4b26791ccab54b06c8c39ad96ffe
Anders Rognstad <anders.rognstad@nav.no>
2021-12-15 09:44:47 +0100

    Remove unneccesary logging

--- src/main/kotlin/no/nav/syfo/dialogmelding/kafka/DialogmeldingConsumerService.kt
+++ src/main/kotlin/no/nav/syfo/dialogmelding/kafka/DialogmeldingConsumerService.kt
@@ -4,8 +4,6 @@ import no.nav.syfo.application.ApplicationState
 import no.nav.syfo.dialogmelding.DialogmeldingService
 import org.apache.kafka.clients.consumer.ConsumerRecords
 import org.apache.kafka.clients.consumer.KafkaConsumer
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
 import java.time.Duration
 
 class DialogmeldingConsumerService(
@@ -27,15 +25,11 @@ class DialogmeldingConsumerService(
     private fun processDialogmeldingerFromRecords(
         records: ConsumerRecords<String, KafkaDialogmeldingDTO>,
     ) {
-        records.forEach {
-            log.info("Received consumer record with key: ${it.key()}")
-            dialogmeldingService.handleDialogmelding(it.value())
-        }
+        records.forEach { dialogmeldingService.handleDialogmelding(it.value()) }
     }
 
     companion object {
         private const val DIALOGMELDING_TOPIC = "teamsykefravr.dialogmelding"
         private const val POLL_DURATION_SECONDS = 1L
-        private val log: Logger = LoggerFactory.getLogger(DialogmeldingConsumerService::class.java)
     }
 }
