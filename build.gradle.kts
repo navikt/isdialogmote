817c444b77b443d9d231696034736dd13bc4afdb
geir-waagboe <geir.arne.waagbo@nav.no>
2022-06-17 12:48:59 +0200

    Use Kotlin 1.7.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,7 +38,7 @@ object Versions {
 }
 
 plugins {
-    kotlin("jvm") version "1.6.20"
+    kotlin("jvm") version "1.7.0"
     id("com.github.johnrengelman.shadow") version "7.1.2"
     id("org.jlleitschuh.gradle.ktlint") version "10.2.1"
 }
