2e29faf3e69d4aa8b4b6d50278108c43cdde2d00
John Martin Lindseth <john.martin.lindseth@nav.no>
2022-09-19 15:26:26 +0200

    Censor referat sent to wrong person (#353)

---
+++ src/main/resources/db/migration/V8_2__update_wrong_referat_2.sql
@@ -0,0 +1,13 @@
+UPDATE MOTE_REFERAT
+    SET situasjon = '[Teksten er fjernet]',
+        konklusjon = '.',
+        arbeidstaker_oppgave = '.',
+        arbeidsgiver_oppgave = '.',
+        veileder_oppgave = null,
+        document = '[{"key": null,"type": "HEADER_H1","texts": ["Referat fra dialogmøte"],"title": null},{"key": null,"type": "PARAGRAPH","texts": ["Sendt 14. september 2022, kl. 11.49"],"title": null},{"key": null,"type": "HEADER_H2","texts": ["[Arbeidstaker]"],"title": null},{"key": null,"type": "PARAGRAPH","texts": ["F.nr. -"],"title": null},{"key": null,"type": "PARAGRAPH","texts": ["Tirsdag 13. september 2022 kl. 10.00"],"title": "Møtetidspunkt"},{"key": null,"type": "PARAGRAPH","texts": ["Videolink"],"title": "Møtested"},{"key": null,"type": "PARAGRAPH","texts": ["Arbeidstaker: -","Fra NAV: -","Fra arbeidsgiver: -","Behandler: Fastlege -"],"title": "Deltakere i møtet"},{"key": null,"type": "PARAGRAPH","texts": ["[Arbeidsgiver]"],"title": "Arbeidsgiver"},{"key": null,"type": "PARAGRAPH","texts": ["Formålet med dialogmøtet var å oppsummere situasjonen, drøfte mulighetene for å arbeide og legge en plan for tiden framover."],"title": null},{"key": null,"type": "PARAGRAPH","texts": ["Sykdom og diagnose er underlagt taushetsplikt. Derfor er helsen din bare et tema hvis du selv velger å være åpen om den. Av hensyn til personvernet inneholder referatet uansett ikke slike opplysninger. Se artikkel 9, Lov om behandling av personopplysninger."],"title": null},{"key": null,"type": "HEADER_H2","texts": ["Dette skjedde i møtet"],"title": null},{"key": null,"type": "PARAGRAPH","texts": ["."],"title": "Konklusjon"},{"key": null,"type": "PARAGRAPH","texts": ["."],"title": "Din oppgave"},{"key": null,"type": "PARAGRAPH","texts": ["."],"title": "Arbeidsgiverens oppgave"},{"key": null,"type": "PARAGRAPH","texts": ["."],"title": "Behandlerens oppgave"},{"key": null,"type": "PARAGRAPH","texts": ["."],"title": "NAVs oppgave"},{"key": null,"type": "PARAGRAPH","texts": ["[Teksten er fjernet]"],"title": "Situasjon og muligheter"},{"key": null,"type": "HEADER_H2","texts": ["Dette informerte NAV om i møtet"],"title": null},{"key": "IKKE_BEHOV","type": "PARAGRAPH","texts": ["[Teksten er fjernet]"],"title": "Ikke behov for bistand fra NAV nå"},{"key": null,"type": "PARAGRAPH","texts": ["Vennlig hilsen","[Veileder]","NAV"],"title": null}]',
+        behandler_oppgave = null
+    WHERE uuid = '253ef1dd-dc2d-4314-9b84-a98b028bad2b';
+
+UPDATE PDF
+    SET pdf = ''::bytea
+    WHERE uuid = '146dded4-b432-46a5-bd8a-c1b0e5ddb7d5';
