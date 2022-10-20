d0c44363fc8b0d3c527d87bfa7d8adeb90b84eb8
Geir Arne Waagbø <79845894+geir-waagboe@users.noreply.github.com>
2022-10-20 09:45:22 +0200

    Cronjob for lukking av dialogmoter (#374)

---
+++ src/main/kotlin/no/nav/syfo/cronjob/dialogmoteOutdated/DialogmoteOutdatedCronjob.kt
@@ -0,0 +1,41 @@
+package no.nav.syfo.cronjob.dialogmoteOutdated
+
+import net.logstash.logback.argument.StructuredArguments
+import no.nav.syfo.application.database.DatabaseInterface
+import no.nav.syfo.cronjob.*
+import org.slf4j.LoggerFactory
+import java.time.LocalDate
+
+class DialogmoteOutdatedCronjob(
+    database: DatabaseInterface,
+    outdatedDialogmoterCutoff: LocalDate,
+) : DialogmoteCronjob {
+
+    override val initialDelayMinutes: Long = 2
+    override val intervalDelayMinutes: Long = 60 * 24
+
+    override suspend fun run() {
+        val outdatedResult = DialogmoteCronjobResult()
+
+        dialogmoteOutdatedJob(outdatedResult)
+
+        COUNT_CRONJOB_OUTDATED_DIALOGMOTE_UPDATE.increment(outdatedResult.updated.toDouble())
+        COUNT_CRONJOB_OUTDATED_DIALOGMOTE_FAIL.increment(outdatedResult.failed.toDouble())
+
+        log.info(
+            "Completed outdated processing with result: {}, {}",
+            StructuredArguments.keyValue("failed", outdatedResult.failed),
+            StructuredArguments.keyValue("updated", outdatedResult.updated),
+        )
+    }
+
+    fun dialogmoteOutdatedJob(
+        outdatedResult: DialogmoteCronjobResult,
+    ) {
+        // TODO:
+    }
+
+    companion object {
+        private val log = LoggerFactory.getLogger(DialogmoteOutdatedCronjob::class.java)
+    }
+}
