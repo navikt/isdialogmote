3013246940dfd1347de76a3554e2b5fad4226d76
Eirik Dahlen <dahleneirik@gmail.com>
2022-11-14 15:17:43 +0100

    Add avro serializer for pdl-aktor

--- src/main/kotlin/no/nav/syfo/identhendelse/kafka/KafkaIdenthendelseConsumerConfig.kt
+++ src/main/kotlin/no/nav/syfo/identhendelse/kafka/KafkaIdenthendelseConsumerConfig.kt
@@ -1,5 +1,7 @@
 package no.nav.syfo.identhendelse.kafka
 
+import io.confluent.kafka.serializers.KafkaAvroDeserializer
+import io.confluent.kafka.serializers.KafkaAvroDeserializerConfig
 import no.nav.syfo.application.ApplicationEnvironmentKafka
 import no.nav.syfo.application.kafka.commonKafkaAivenConfig
 import org.apache.kafka.clients.consumer.ConsumerConfig
@@ -13,10 +15,15 @@ fun kafkaIdenthendelseConsumerConfig(
         putAll(commonKafkaAivenConfig(applicationEnvironmentKafka))
         this[ConsumerConfig.GROUP_ID_CONFIG] = "isdialogmote-v0" // Dette er for å kjøre dryrun. Oppdateres til v1 når alt ser "good" ut
         this[ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG] = StringDeserializer::class.java.canonicalName
-        this[ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG] = KafkaIdenthendelseDeserializer::class.java.canonicalName
+        this[ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG] = KafkaAvroDeserializer::class.java.canonicalName
         this[ConsumerConfig.AUTO_OFFSET_RESET_CONFIG] = "earliest"
         this[ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG] = "false"
         this[ConsumerConfig.MAX_POLL_RECORDS_CONFIG] = "1" // Tanken er at vi bare henter én og én endring
         this[ConsumerConfig.MAX_PARTITION_FETCH_BYTES_CONFIG] = "" + (10 * 1024 * 1024)
+
+        this[KafkaAvroDeserializerConfig.SCHEMA_REGISTRY_URL_CONFIG] = applicationEnvironmentKafka.aivenSchemaRegistryUrl
+        this[KafkaAvroDeserializerConfig.SPECIFIC_AVRO_READER_CONFIG] = false
+        this[KafkaAvroDeserializerConfig.USER_INFO_CONFIG] = "${applicationEnvironmentKafka.aivenRegistryUser}:${applicationEnvironmentKafka.aivenRegistryPassword}"
+        this[KafkaAvroDeserializerConfig.BASIC_AUTH_CREDENTIALS_SOURCE] = "USER_INFO"
     }
 }
