af517a3dd5fdcfc490ee6b55f383bdd08f616402
Solgård <vetle.bu.solgard@nav.no>
2024-02-05 13:59:43 +0100

    IS-1947: Refactor tests

--- src/test/kotlin/no/nav/syfo/dialogmote/api/v2/TildelDialogmoteApiV2Spek.kt
+++ src/test/kotlin/no/nav/syfo/dialogmote/api/v2/TildelDialogmoteApiV2Spek.kt
@@ -18,7 +18,6 @@ import no.nav.syfo.dialogmote.api.domain.TildelDialogmoterDTO
 import no.nav.syfo.dialogmote.database.createNewDialogmoteWithReferences
 import no.nav.syfo.testhelper.*
 import no.nav.syfo.testhelper.generator.generateNewDialogmote
-import no.nav.syfo.testhelper.generator.generateNewDialogmoteDTO
 import no.nav.syfo.util.bearerHeader
 import no.nav.syfo.util.configuredJacksonMapper
 import org.amshove.kluent.shouldBeEqualTo
@@ -75,31 +74,14 @@ class TildelDialogmoteApiV2Spek : Spek({
                 val urlMote = "$dialogmoteApiV2Basepath$dialogmoteApiPersonIdentUrlPath"
                 val urlMoterEnhet = "$dialogmoteApiV2Basepath$dialogmoteApiEnhetUrlPath/${UserConstants.ENHET_NR.value}"
                 val urlTildelMote = "$dialogmoteApiV2Basepath$dialogmoteTildelPath"
-                val newDialogmoteDTO = generateNewDialogmoteDTO(UserConstants.ARBEIDSTAKER_FNR)
-                val newDialogmoteDTOAnnenArbeidstaker = generateNewDialogmoteDTO(UserConstants.ARBEIDSTAKER_ANNEN_FNR)
+                val newDialogmote = generateNewDialogmote(UserConstants.ARBEIDSTAKER_FNR)
+                val newDialogmoteAnnenArbeidstaker = generateNewDialogmote(UserConstants.ARBEIDSTAKER_ANNEN_FNR)
 
                 describe("Happy path") {
                     it("should tildele dialogmoter if request is successful") {
                         val createdDialogmoterUuids = mutableListOf<UUID>()
-
-                        with(
-                            handleRequest(HttpMethod.Post, urlMote) {
-                                addHeader(HttpHeaders.Authorization, bearerHeader(veilederCallerToken))
-                                addHeader(HttpHeaders.ContentType, ContentType.Application.Json.toString())
-                                setBody(objectMapper.writeValueAsString(newDialogmoteDTO))
-                            }
-                        ) {
-                            response.status() shouldBeEqualTo HttpStatusCode.OK
-                        }
-                        with(
-                            handleRequest(HttpMethod.Post, urlMote) {
-                                addHeader(HttpHeaders.Authorization, bearerHeader(veilederCallerToken))
-                                addHeader(HttpHeaders.ContentType, ContentType.Application.Json.toString())
-                                setBody(objectMapper.writeValueAsString(newDialogmoteDTOAnnenArbeidstaker))
-                            }
-                        ) {
-                            response.status() shouldBeEqualTo HttpStatusCode.OK
-                        }
+                        database.connection.run { this.createNewDialogmoteWithReferences(newDialogmote) }
+                        database.connection.run { this.createNewDialogmoteWithReferences(newDialogmoteAnnenArbeidstaker) }
 
                         with(
                             handleRequest(HttpMethod.Get, urlMoterEnhet) {
@@ -209,16 +191,7 @@ class TildelDialogmoteApiV2Spek : Spek({
 
                     it("should return status Forbidden if contains dialogmøte with denied access to person") {
                         val createdDialogmoterUuids = mutableListOf<UUID>()
-
-                        with(
-                            handleRequest(HttpMethod.Post, urlMote) {
-                                addHeader(HttpHeaders.Authorization, bearerHeader(veilederCallerToken))
-                                addHeader(HttpHeaders.ContentType, ContentType.Application.Json.toString())
-                                setBody(objectMapper.writeValueAsString(newDialogmoteDTO))
-                            }
-                        ) {
-                            response.status() shouldBeEqualTo HttpStatusCode.OK
-                        }
+                        database.connection.run { this.createNewDialogmoteWithReferences(newDialogmote) }
 
                         with(
                             handleRequest(HttpMethod.Get, urlMoterEnhet) {
