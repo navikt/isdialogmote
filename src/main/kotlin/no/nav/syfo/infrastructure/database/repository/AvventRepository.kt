c43b3ffed0daa64e3d14e85d0664a12c1be3c523
Daniel Engebretsen <daniel.engebretsen@nav.no>
2026-03-02 13:31:38 +0100

    AvventApiTest

--- src/main/kotlin/no/nav/syfo/infrastructure/database/repository/AvventRepository.kt
+++ src/main/kotlin/no/nav/syfo/infrastructure/database/repository/AvventRepository.kt
@@ -8,24 +8,30 @@ import no.nav.syfo.infrastructure.database.DatabaseInterface
 import java.sql.ResultSet
 import java.util.UUID
 
-class AvventRepository(private val database: DatabaseInterface) : IAvventRepository {
-    override fun persist(avvent: Avvent, transaction: ITransaction?) {
+class AvventRepository(
+    private val database: DatabaseInterface,
+) : IAvventRepository {
+    override fun persist(
+        avvent: Avvent,
+        transaction: ITransaction?,
+    ) {
         val connection = transaction?.connection ?: database.connection
-        connection.prepareStatement(
-            """
+        connection
+            .prepareStatement(
+                """
                 INSERT INTO avvent (uuid, created_at, frist, created_by, personident, beskrivelse, is_lukket)
                 VALUES (?, ?, ?, ?, ?, ?, ?)
-                """.trimIndent()
-        ).use { preparedStatement ->
-            preparedStatement.setObject(1, avvent.uuid)
-            preparedStatement.setObject(2, avvent.createdAt)
-            preparedStatement.setObject(3, avvent.frist)
-            preparedStatement.setString(4, avvent.createdBy)
-            preparedStatement.setString(5, avvent.personident.value)
-            preparedStatement.setString(6, avvent.beskrivelse)
-            preparedStatement.setBoolean(7, avvent.isLukket)
-            preparedStatement.executeUpdate()
-        }
+                """.trimIndent(),
+            ).use { preparedStatement ->
+                preparedStatement.setObject(1, avvent.uuid)
+                preparedStatement.setObject(2, avvent.createdAt)
+                preparedStatement.setObject(3, avvent.frist)
+                preparedStatement.setString(4, avvent.createdBy)
+                preparedStatement.setString(5, avvent.personident.value)
+                preparedStatement.setString(6, avvent.beskrivelse)
+                preparedStatement.setBoolean(7, avvent.isLukket)
+                preparedStatement.executeUpdate()
+            }
         if (transaction == null) {
             connection.commit()
             connection.close()
@@ -34,44 +40,50 @@ class AvventRepository(private val database: DatabaseInterface) : IAvventReposit
 
     override fun getAvvent(uuid: UUID): Avvent? {
         database.connection.use { connection ->
-            connection.prepareStatement(
-                """
-                SELECT * FROM avvent WHERE uuid = ?
-                """.trimIndent()
-            ).use { preparedStatement ->
-                preparedStatement.setObject(1, uuid)
-                preparedStatement.executeQuery().use { resultSet ->
-                    return if (resultSet.next()) resultSet.toAvvent() else null
+            connection
+                .prepareStatement(
+                    """
+                    SELECT * FROM avvent WHERE uuid = ?
+                    """.trimIndent(),
+                ).use { preparedStatement ->
+                    preparedStatement.setObject(1, uuid)
+                    preparedStatement.executeQuery().use { resultSet ->
+                        return if (resultSet.next()) resultSet.toAvvent() else null
+                    }
                 }
-            }
         }
     }
 
     override fun getActiveAvvent(personident: PersonIdent): Avvent? {
         database.connection.use { connection ->
-            connection.prepareStatement(
-                """
-                SELECT * FROM avvent WHERE personident = ? AND is_lukket = false
-                """.trimIndent()
-            ).use { preparedStatement ->
-                preparedStatement.setString(1, personident.value)
-                preparedStatement.executeQuery().use { resultSet ->
-                    return if (resultSet.next()) resultSet.toAvvent() else null
+            connection
+                .prepareStatement(
+                    """
+                    SELECT * FROM avvent WHERE personident = ? AND is_lukket = false
+                    """.trimIndent(),
+                ).use { preparedStatement ->
+                    preparedStatement.setString(1, personident.value)
+                    preparedStatement.executeQuery().use { resultSet ->
+                        return if (resultSet.next()) resultSet.toAvvent() else null
+                    }
                 }
-            }
         }
     }
 
-    override fun setLukket(uuid: UUID, transaction: ITransaction?) {
+    override fun setLukket(
+        uuid: UUID,
+        transaction: ITransaction?,
+    ) {
         val connection = transaction?.connection ?: database.connection
-        connection.prepareStatement(
-            """
+        connection
+            .prepareStatement(
+                """
                 UPDATE avvent SET is_lukket = true WHERE uuid = ?
-                """.trimIndent()
-        ).use { preparedStatement ->
-            preparedStatement.setObject(1, uuid)
-            preparedStatement.executeUpdate()
-        }
+                """.trimIndent(),
+            ).use { preparedStatement ->
+                preparedStatement.setObject(1, uuid)
+                preparedStatement.executeUpdate()
+            }
         if (transaction == null) {
             connection.commit()
             connection.close()
@@ -79,12 +91,13 @@ class AvventRepository(private val database: DatabaseInterface) : IAvventReposit
     }
 }
 
-private fun ResultSet.toAvvent() = Avvent(
-    uuid = this.getObject("uuid", UUID::class.java),
-    createdAt = this.getObject("created_at", java.time.OffsetDateTime::class.java),
-    frist = this.getObject("frist", java.time.LocalDate::class.java),
-    createdBy = this.getString("created_by"),
-    personident = PersonIdent(this.getString("personident")),
-    beskrivelse = this.getString("beskrivelse"),
-    isLukket = this.getBoolean("is_lukket")
-)
+private fun ResultSet.toAvvent() =
+    Avvent(
+        uuid = this.getObject("uuid", UUID::class.java),
+        createdAt = this.getObject("created_at", java.time.OffsetDateTime::class.java),
+        frist = this.getObject("frist", java.time.LocalDate::class.java),
+        createdBy = this.getString("created_by"),
+        personident = PersonIdent(this.getString("personident")),
+        beskrivelse = this.getString("beskrivelse"),
+        isLukket = this.getBoolean("is_lukket"),
+    )
