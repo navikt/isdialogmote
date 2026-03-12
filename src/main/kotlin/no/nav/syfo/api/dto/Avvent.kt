d7ad50b5e3160ec98bc59a8481a3e4b56a15725a
Daniel Engebretsen <daniel.engebretsen@nav.no>
2026-03-12 10:29:05 +0100

    remove extension function

--- src/main/kotlin/no/nav/syfo/api/dto/Avvent.kt
+++ src/main/kotlin/no/nav/syfo/api/dto/Avvent.kt
@@ -10,20 +10,19 @@ data class CreateAvventDTO(
     val frist: LocalDate,
     val personident: String,
     val beskrivelse: String,
-)
+) {
+    fun toAvvent(createdBy: String): Avvent =
+        Avvent(
+            uuid = UUID.randomUUID(),
+            createdAt = OffsetDateTime.now(),
+            frist = this.frist,
+            createdBy = createdBy,
+            personident = PersonIdent(this.personident),
+            beskrivelse = this.beskrivelse,
+            isLukket = false,
+        )
+}
 
 data class QueryAvventDTO(
     val personidenter: List<String>,
 )
-
-fun CreateAvventDTO.toAvvent(createdBy: String): Avvent {
-    return Avvent(
-        uuid = UUID.randomUUID(),
-        createdAt = OffsetDateTime.now(),
-        frist = this.frist,
-        createdBy = createdBy,
-        personident = PersonIdent(this.personident),
-        beskrivelse = this.beskrivelse,
-        isLukket = false
-    )
-}
