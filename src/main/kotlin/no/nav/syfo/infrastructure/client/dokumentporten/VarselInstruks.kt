ae14de813e76d56d9dc6c941733a2c06aa12a689
Bjørn Faaberg <bjorn.faaberg@nav.no>
2026-06-01 07:50:13 +0200

    Justeringer på tekstene vil VarselInstruks

--- src/main/kotlin/no/nav/syfo/infrastructure/client/dokumentporten/VarselInstruks.kt
+++ src/main/kotlin/no/nav/syfo/infrastructure/client/dokumentporten/VarselInstruks.kt
@@ -6,60 +6,77 @@ import no.nav.syfo.domain.dialogmote.MotedeltakerVarselType
 import no.nav.syfo.infrastructure.client.ereg.EregVirksomhetsnavn
 
 private const val KILDE = "isdialogmote"
-private const val VARSEL_TEKST_INNKALT = "Innkalling til dialogmøte med Nav"
-private const val VARSEL_TEKST_AVLYST = "Dialogmøte med Nav er avlyst"
-private const val VARSEL_TEKST_EMAIL_TITTEL_NYTT_TID_STED = "Dialogmøte med Nav er endret"
+private const val VARSEL_TEKST_INNKALT = "Viktig: Innkalling til dialogmøte med Nav"
+private const val VARSEL_TEKST_AVLYST = "Viktig: Dialogmøte med Nav er avlyst"
+private const val VARSEL_TEKST_EMAIL_TITTEL_NYTT_TID_STED = "Viktig: Dialogmøte med Nav er endret"
 private const val VARSEL_TEKST_REFERAT = "Referat fra dialogmøte med Nav"
 
-private const val EMAIL_TITTEL_INNKALT = "Innkalling til dialogmøte med Nav"
-private const val EMAIL_TITTEL_NYTT_TID_STED = "Dialogmøte med Nav er endret"
-private const val EMAIL_TITTEL_AVLYST = "Dialogmøte med Nav er avlyst"
-private const val EMAIL_TITTEL_REFERAT = "Referat fra dialogmøte med Nav"
+private const val EMAIL_TITTEL_INNKALT = "Viktig: Innkalling til dialogmøte med Nav - {mottaker}"
+private const val EMAIL_TITTEL_NYTT_TID_STED = "Viktig: Dialogmøte med Nav er endret - {mottaker}"
+private const val EMAIL_TITTEL_AVLYST = "Viktig: Dialogmøte med Nav er avlyst - {mottaker}"
+private const val EMAIL_TITTEL_REFERAT = "Referat fra dialogmøte med Nav - {mottaker}"
 private const val SIGNATUR = "Vennlig hilsen Nav"
 
 private const val MOTTAKER_PLACEHOLDER = "{mottaker}"
 
 private val EMAIL_BODY_INNKALT = """
-    <p>{mottaker} er innkalt til dialogmøte med Nav i forbindelse med
-    sykefraværet til en av deres ansatte.</p>
+    <p>Hei,
+    Nav har kalt inn til et dialogmøte for en av deres ansatte. Når nærmeste leder ikke er registrert hos Nav, går innkallingen til bedriften.
+    Du mottar denne beskjeden fordi du er satt opp for å motta varsler om dialogmøter i Altinn for {mottaker}.
 
-    <p>Du kan lese innkallingen i {mottaker} sin innboks i Altinn, eller ved å logge inn på Min side arbeidsgiver hos Nav.</p>
+    <p>Hva må du gjøre nå?</p>
+    <ul>
+        <li>Logg deg inn i Altinn eller på Min side arbeidsgiver hos Nav for å lese innkalling.</li>
+        <li>Sørg for at lederen som skal delta i møtet får videresendt innkalling.</li>
+        <li>Vurder å få ditt firma til å registrere nærmeste leder for den sykmeldte</li>
+    </ul>
 
     <p>$SIGNATUR</p>
-"""
-
+""".trimIndent()
 private val EMAIL_BODY_NYTT_TID_STED = """
-    <p>{mottaker} er innkalt til dialogmøte med Nav i forbindelse med
-    sykefraværet til en av deres ansatte.</p>
+    <p>Hei,
+    Nav har flyttet tid eller sted for et dialogmøte for en av deres ansatte. Når nærmeste leder ikke er registrert hos Nav, går endringen til bedriften.
+    Du mottar denne beskjeden fordi du er satt opp for å motta varsler om dialogmøter i Altinn for {mottaker}.
 
-    <p>Nav har endret tidspunktet eller stedet for dialogmøtet.</p>
-
-    <p>Du kan lese endringen i {mottaker} sin innboks i Altinn, eller ved å logge inn på Min side arbeidsgiver hos Nav.</p>
+    <p>Hva må du gjøre nå?</p>
+    <ul>
+        <li>Logg deg inn i Altinn eller på Min side arbeidsgiver hos Nav for å lese endringen.</li>
+        <li>Sørg for at lederen som skal delta i møtet får videresendt endringen.</li>
+        <li>Vurder å få ditt firma til å registrere nærmeste leder for den sykmeldte</li>
+    </ul>
 
     <p>$SIGNATUR</p>
-"""
+""".trimIndent()
 
 private val EMAIL_BODY_AVLYST = """
-    <p>{mottaker} var kalt inn til et dialogmøte med Nav i forbindelse med
-    sykefraværet til en av deres ansatte.</p>
+    <p>Hei,
+    Nav har avlyst et dialogmøte for en av deres ansatte. Når nærmeste leder ikke er registrert hos Nav, går avlysningen til bedriften.
+    Du mottar denne beskjeden fordi du er satt opp for å motta varsler om dialogmøter i Altinn for {mottaker}.
 
-    <p>Dialogmøtet har blitt avlyst.</p>
-
-    <p> Du kan lese avlysningen i {mottaker} sin innboks i Altinn, eller ved å logge inn på Min side arbeidsgiver hos Nav.</p>
-
+    <p>Hva må du gjøre nå?</p>
+    <ul>
+        <li>Logg deg inn i Altinn eller på Min side arbeidsgiver hos Nav for å lese avlysningen.</li>
+        <li>Sørg for at lederen som skal delta i møtet får videresendt avlysningen.</li>
+        <li>Vurder å få ditt firma til å registrere nærmeste leder for den sykmeldte</li>
+    </ul>
+    
     <p>$SIGNATUR</p>
-"""
+""".trimIndent()
 
 private val EMAIL_BODY_REFERAT = """
-    <p>{mottaker} har vært i dialogmøte med Nav i forbindelse med
-    sykefraværet til en av deres ansatte.</p>
+    <p>Hei,
+    Nav har sendt referat fra et dialogmøte for en av deres ansatte. Når nærmeste leder ikke er registrert hos Nav, går referatet til bedriften.
+    Du mottar denne beskjeden fordi du er satt opp for å motta varsler om dialogmøter i Altinn for {mottaker}.
+
+    <p>Hva må du gjøre nå?</p>
+    <ul>
+        <li>Logg deg inn i Altinn eller på Min side arbeidsgiver hos Nav for å lese referatet.</li>
+        <li>Sørg for at lederen som skal delta i møtet får videresendt avlysningen.</li>
+        <li>Vurder å få ditt firma til å registrere nærmeste leder for den sykmeldte</li>
+    </ul>
     
-    <p>Referatet er nå tilgjengelig.</p>
-
-    <p>Du kan lese referatet i {mottaker} sin innboks i Altinn, eller ved å logge inn på Min side arbeidsgiver hos Nav.</p>
-
     <p>$SIGNATUR</p>
-"""
+""".trimIndent()
 
 private val SMS_BODY_INNKALT = """
     {mottaker} er innkalt til dialogmøte med Nav i forbindelse med
@@ -87,7 +104,7 @@ private val SMS_BODY_AVLYST = """
 
     Dialogmøtet har blitt avlyst.
 
-     Du kan lese avlysningen i {mottaker} sin innboks i Altinn, eller ved å logge inn på Min side arbeidsgiver hos Nav.
+    Du kan lese avlysningen i {mottaker} sin innboks i Altinn, eller ved å logge inn på Min side arbeidsgiver hos Nav.
 
     $SIGNATUR
 """.trimIndent()
@@ -96,30 +113,36 @@ private val SMS_BODY_REFERAT = """
     {mottaker} har vært i dialogmøte med Nav i forbindelse med
     sykefraværet til en av deres ansatte.
     
-     Referatet er nå tilgjengelig.
+    Referatet er nå tilgjengelig.
 
-     Du kan lese referatet i {mottaker} sin innboks i Altinn, eller ved å logge inn på Min side arbeidsgiver hos Nav.
+    Du kan lese referatet i {mottaker} sin innboks i Altinn, eller ved å logge inn på Min side arbeidsgiver hos Nav.
 
     $SIGNATUR
 """.trimIndent()
 
-private fun toEmailTitle(varseltype: MotedeltakerVarselType): String {
-    return when (varseltype) {
+private fun toEmailTitle(
+    varseltype: MotedeltakerVarselType,
+    virksomhetsnummer: Virksomhetsnummer,
+    virksomhetsnavn: EregVirksomhetsnavn?
+): String {
+    val template = when (varseltype) {
         MotedeltakerVarselType.INNKALT -> EMAIL_TITTEL_INNKALT
         MotedeltakerVarselType.NYTT_TID_STED -> EMAIL_TITTEL_NYTT_TID_STED
         MotedeltakerVarselType.AVLYST -> EMAIL_TITTEL_AVLYST
         MotedeltakerVarselType.REFERAT -> EMAIL_TITTEL_REFERAT
     }
+    return template.withMottaker(toMottaker(virksomhetsnummer, virksomhetsnavn))
 }
 
-private fun toVarselTekst(varseltype: MotedeltakerVarselType): String {
-    return when (varseltype) {
+private fun toVarselTekst(
+    varseltype: MotedeltakerVarselType,
+): String =
+    when (varseltype) {
         MotedeltakerVarselType.INNKALT -> VARSEL_TEKST_INNKALT
         MotedeltakerVarselType.NYTT_TID_STED -> VARSEL_TEKST_EMAIL_TITTEL_NYTT_TID_STED
         MotedeltakerVarselType.AVLYST -> VARSEL_TEKST_AVLYST
         MotedeltakerVarselType.REFERAT -> VARSEL_TEKST_REFERAT
     }
-}
 
 private fun toMottaker(
     virksomhetsnummer: Virksomhetsnummer,
@@ -181,7 +204,7 @@ data class VarselInstruks(
             return VarselInstruks(
                 type = HendelseType.AG_VARSEL_ALTINN_RESSURS,
                 notifikasjonInnhold = NotifikasjonInnhold(
-                    epostTittel = toEmailTitle(varselType),
+                    epostTittel = toEmailTitle(varselType, virksomhetsnummer, virksomhetsnavn),
                     epostBody = toEmailBody(varselType, virksomhetsnummer, virksomhetsnavn),
                     smsTekst = toSMSBody(varselType, virksomhetsnummer, virksomhetsnavn),
                     varselTekst = toVarselTekst(varselType)
